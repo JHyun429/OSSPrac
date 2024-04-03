1b659b543ba244ebbff3f09c8671624fad8149c5
JHyun429 <2022112408@dgu.ac.kr>
2024-04-03 22:29:44 +0900

    2024-04-03 개인과제5 2번

--- prac5.java
+++ prac5.java
@@ -7,15 +7,18 @@ public class prac5 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("이름을 입력하세요: ");
 		System.out.println("학번을 입력하세요: ");
+		System.out.println("학과를 입력하세요: ");
 		
 		String name = sc.next();
 		int num = sc.nextInt();
+		String major = sc.next();
 		
 		sc.close();
 		
 		System.out.println("<출력>");
 		System.out.println("이름: " + name);
 		System.out.println("학번: " + num);
+		System.out.println("학과: " + major);
 		
 
 	}
