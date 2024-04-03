c54fa50c7ca45fc30d1697ee1dad356e1e6a6483
JHyun429 <2022112408@dgu.ac.kr>
2024-04-03 16:18:26 +0900

    pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("hello world!")
 name = "gdhong"
 print(f'I\'m GD Hong!')
-programming_language = "Python"
+programming_language = "Python and Java"
 print("Most used language is {0}".format(programming_language))
