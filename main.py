b7c08922f1acacc508dbad40305cd45e187b832f
JHyun429 <2022112408@dgu.ac.kr>
2024-04-03 15:24:32 +0900

    M main.py jhoh

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 print("hello world!")
-name = "gdhong"
-print(f'I\'m GD Hong!')
-programming_language = "Python"
-print("Most used language is {0}".format(programming_language))
+name = input("이름: ")
+print("환영" + name + "님!")
