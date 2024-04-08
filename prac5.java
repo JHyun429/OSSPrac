19e531b43c046ec8cb4ed1edd76465e43212d17c
JHyun429 <2022112408@dgu.ac.kr>
2024-04-08 13:21:25 +0900

    M java ossp2

--- prac5.java
+++ prac5.java
@@ -6,19 +6,24 @@ public class prac5 {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("이름을 입력하세요: ");
-		System.out.println("학번을 입력하세요: ");
-		System.out.println("학과를 입력하세요: ");
-		
 		String name = sc.next();
+		
+		System.out.println("학번을 입력하세요: ");
 		int num = sc.nextInt();
+		
+		System.out.println("학과를 입력하세요: ");
 		String major = sc.next();
 		
+		System.out.println("학년를 입력하세요: ");
+		int schYear = sc.next();
+
 		sc.close();
 		
 		System.out.println("<출력>");
 		System.out.println("이름: " + name);
 		System.out.println("학번: " + num);
 		System.out.println("학과: " + major);
+		System.out.println("학년: " + schYear);
 
 	}
 
